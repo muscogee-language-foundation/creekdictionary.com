154c10b6b42d3699a6403f5b61ded81f4439916c
Adam Recvlohe <adam.recvlohe@gmail.com>
2019-06-24 21:19:37 -0500

    Add more words and  improve UI

--- src/pages/Home.re
+++ src/pages/Home.re
@@ -1,6 +1,6 @@
 [@react.component]
 let make = () => {
-  <section className="pb-16 pt-64 px-6 flex justify-center">
+  <section className="pb-16 pt-32 sm:pt-64 px-6 flex justify-center">
     <Search />
   </section>;
 };
--- src/words.js
+++ src/words.js
@@ -12,7 +12,181 @@ const words = [
     tags: "animal,clan,bear"
   },
   { english: "white", creek: "hvtke", meaning: "white", tags: "color" },
-  { english: "black", creek: "lvste", meaning: "black", tags: "color" }
+  { english: "black", creek: "lvste", meaning: "black", tags: "color" },
+  { english: "man", creek: "honvnwv", meaning: "man", tags: "people" },
+  { english: "woman", creek: "hokte", meaning: "woman", tags: "people" },
+  { english: "boy", creek: "cepane", meaning: "boy", tags: "people" },
+  { english: "girl", creek: "hoktuce", meaning: "girl", tags: "people" },
+  { english: "child", creek: "estuce", meaning: "child", tags: "people" },
+  { english: "baby", creek: "pipuce", meaning: "baby", tags: "people" },
+  {
+    english: "old man",
+    creek: "este vcule",
+    meaning: "old man",
+    tags: "people"
+  },
+  {
+    english: "old woman",
+    creek: "hoktvle",
+    meaning: "old woman",
+    tags: "people"
+  },
+  {
+    english: "married man",
+    creek: "honvnwv ehicv",
+    meaning: "married man",
+    tags: "people"
+  },
+  {
+    english: "married woman",
+    creek: "hokte ehesse",
+    meaning: "married woman",
+    tags: "people"
+  },
+  {
+    english: "one",
+    creek: "hvmken",
+    meaning: "the number one",
+    tags: "number"
+  },
+  {
+    english: "two",
+    creek: "hokkolen",
+    meaning: "the number two",
+    tags: "number"
+  },
+  {
+    english: "three",
+    creek: "tutcenen",
+    meaning: "the number three",
+    tags: "number"
+  },
+  {
+    english: "four",
+    creek: "osten",
+    meaning: "the number four",
+    tags: "number"
+  },
+  {
+    english: "five",
+    creek: "cvhkepen",
+    meaning: "the number five",
+    tags: "number"
+  },
+  {
+    english: "six",
+    creek: "epaken",
+    meaning: "the number six",
+    tags: "number"
+  },
+  {
+    english: "seven",
+    creek: "kolvpaken",
+    meaning: "the number seven",
+    tags: "number"
+  },
+  {
+    english: "eight",
+    creek: "cenvpaken",
+    meaning: "the number eight",
+    tags: "number"
+  },
+  {
+    english: "nine",
+    creek: "ostvpaken",
+    meaning: "the number nine",
+    tags: "number"
+  },
+  {
+    english: "ten",
+    creek: "palen",
+    meaning: "the number ten",
+    tags: "number"
+  },
+  {
+    english: "cow",
+    creek: "wakv",
+    meaning: "cow",
+    tags: "animal"
+  },
+  {
+    english: "horse",
+    creek: "rakko",
+    meaning: "horse",
+    tags: "animal"
+  },
+  {
+    english: "dog",
+    creek: "efv",
+    meaning: "dog",
+    tags: "animal"
+  },
+  {
+    english: "cat",
+    creek: "pose",
+    meaning: "cat",
+    tags: "animal"
+  },
+  {
+    english: "pig",
+    creek: "sokhv",
+    meaning: "pig",
+    tags: "animal"
+  },
+  {
+    english: "chicken",
+    creek: "tolose",
+    meaning: "chicken",
+    tags: "animal"
+  },
+  {
+    english: "goose",
+    creek: "sasvkwv",
+    meaning: "goose",
+    tags: "animal"
+  },
+  {
+    english: "mouse",
+    creek: "cesse",
+    meaning: "mouse",
+    tags: "animal"
+  },
+  {
+    english: "fish",
+    creek: "rvro",
+    meaning: "fish",
+    tags: "animal"
+  },
+  {
+    english: "deer",
+    creek: "eco",
+    meaning: "deer",
+    tags: "animal,clan"
+  },
+  {
+    english: "alligator",
+    creek: "hvlpvtv",
+    meaning: "alligator",
+    tags: "animal,clan"
+  },
+  {
+    english: "tiger",
+    creek: "kaccv",
+    meaning: "tiger",
+    tags: "animal,clan"
+  },
+  {
+    english: "racoon",
+    creek: "wotko",
+    meaning: "racoon",
+    tags: "animal,clan"
+  },
+  {
+    english: "beaver",
+    creek: "ecaswv",
+    meaning: "beaver",
+    tags: "animal,clan"
+  }
 ];
 
 const json = JSON.stringify(words);
