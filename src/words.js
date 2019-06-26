a2900db73ceb8e0bad20cd61ba19e3c084c627df
Adam Recvlohe <adam.recvlohe@gmail.com>
2019-06-25 22:02:25 -0500

    [Feature] Add more words (#5)

--- src/words.js
+++ src/words.js
@@ -186,6 +186,246 @@ const words = [
     creek: "ecaswv",
     meaning: "beaver",
     tags: "animal,clan"
+  },
+  {
+    english: "tree",
+    creek: "eto",
+    meaning: "tree",
+    tags: "nature"
+  },
+  {
+    english: "bush",
+    creek: "stvpokhe",
+    meaning: "bush",
+    tags: "nature"
+  },
+  {
+    english: "hill",
+    creek: "ekvnhvlwuce",
+    meaning: "hill",
+    tags: "nature"
+  },
+  {
+    english: "river",
+    creek: "hvcce",
+    meaning: "river",
+    tags: "nature"
+  },
+  {
+    english: "stream",
+    creek: "hvccuce",
+    meaning: "stream",
+    tags: "nature"
+  },
+  {
+    english: "water",
+    creek: "owv (or wewv, uewv)",
+    meaning: "water",
+    tags: "nature,food"
+  },
+  {
+    english: "rain",
+    creek: "oske",
+    meaning: "rain",
+    tags: "nature"
+  },
+  {
+    english: "sun",
+    creek: "hvse",
+    meaning: "sun",
+    tags: "nature"
+  },
+  {
+    english: "cloud",
+    creek: "vholoce",
+    meaning: "cloud",
+    tags: "nature"
+  },
+  {
+    english: "wind",
+    creek: "hotvle",
+    meaning: "wind",
+    tags: "nature"
+  },
+  {
+    english: "sky",
+    creek: "sotv",
+    meaning: "sky",
+    tags: "nature"
+  },
+  {
+    english: "grass",
+    creek: "pvhe",
+    meaning: "grass",
+    tags: "nature"
+  },
+  {
+    english: "big",
+    creek: "rakke",
+    meaning: "big",
+    tags: "adjective"
+  },
+  {
+    english: "short",
+    creek: "cutkose",
+    meaning: "short",
+    tags: "adjective"
+  },
+  {
+    english: "thick",
+    creek: "cekfe",
+    meaning: "thick",
+    tags: "adjective"
+  },
+  {
+    english: "thin",
+    creek: "tvskoce",
+    meaning: "thin",
+    tags: "adjective"
+  },
+  {
+    english: "thin (person)",
+    creek: "hotosose",
+    meaning: "thin (person)",
+    tags: "adjective"
+  },
+  {
+    english: "tall",
+    creek: "mahe",
+    meaning: "tall",
+    tags: "adjective"
+  },
+  {
+    english: "short",
+    creek: "kocokne",
+    meaning: "short",
+    tags: "adjective"
+  },
+  {
+    english: "loud",
+    creek: "pokhe",
+    meaning: "loud",
+    tags: "adjective"
+  },
+  {
+    english: "quiet",
+    creek: "cvyayvke",
+    meaning: "quiet",
+    tags: "adjective"
+  },
+  {
+    english: "fast",
+    creek: "pvfne",
+    meaning: "fast",
+    tags: "adjective"
+  },
+  {
+    english: "slow",
+    creek: "hvlvlatke",
+    meaning: "slow",
+    tags: "adjective"
+  },
+  {
+    english: "wide",
+    creek: "tvphe",
+    meaning: "wide",
+    tags: "adjective"
+  },
+  {
+    english: "kind",
+    creek: "lopice",
+    meaning: "kind",
+    tags: "adjective"
+  },
+  {
+    english: "mean",
+    creek: "holwvyece",
+    meaning: "mean",
+    tags: "adjective"
+  },
+  {
+    english: "heavy",
+    creek: "honne",
+    meaning: "heavy",
+    tags: "adjective"
+  },
+  {
+    english: "light (weight)",
+    creek: "tvhokne",
+    meaning: "light",
+    tags: "adjective"
+  },
+  {
+    english: "hot",
+    creek: "hiye",
+    meaning: "hot",
+    tags: "adjective"
+  },
+  {
+    english: "cold",
+    creek: "kvsvppe",
+    meaning: "cold",
+    tags: "adjective"
+  },
+  {
+    english: "deep (water)",
+    creek: "lvoke",
+    meaning: "deep",
+    tags: "adjective"
+  },
+  {
+    english: "cahke",
+    creek: "shallow",
+    meaning: "cahke",
+    tags: "adjective"
+  },
+  {
+    english: "strong",
+    creek: "yekce",
+    meaning: "strong",
+    tags: "adjective"
+  },
+  {
+    english: "weak",
+    creek: "lowake",
+    meaning: "weak",
+    tags: "adjective"
+  },
+  {
+    english: "fat",
+    creek: "nehe",
+    meaning: "fat",
+    tags: "adjective"
+  },
+  {
+    english: "tired",
+    creek: "hotose",
+    meaning: "tired",
+    tags: "adjective"
+  },
+  {
+    english: "sad",
+    creek: "feknokke",
+    meaning: "sad",
+    tags: "adjective"
+  },
+  {
+    english: "happy",
+    creek: "afvcke",
+    meaning: "happy",
+    tags: "adjective"
+  },
+  {
+    english: "good",
+    creek: "henre",
+    meaning: "good",
+    tags: "adjective"
+  },
+  {
+    english: "bad",
+    creek: "holwvke",
+    meaning: "bad",
+    tags: "adjective"
   }
 ];
 
