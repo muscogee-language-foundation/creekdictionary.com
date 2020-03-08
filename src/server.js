9c11cad22cdae392b66ba598702e2719dfbd6cde
Adam Recvlohe <adam.recvlohe@gmail.com>
2020-03-07 22:06:24 -0600

    Add manifest and PWA info (#31)

--- src/server.js
+++ src/server.js
@@ -19,6 +19,10 @@ server
     "/assets",
     express.static(path.join(__dirname, "../dist"), { maxAge: "30d" })
   )
+  .use(
+    "/public",
+    express.static(path.join(__dirname, "../public"), { maxAge: "30d" })
+  )
   .use(prerender)
   .get("/*", (_req, res) => {
     const html = ReactDOMServer.renderToString(React.createElement(App));
@@ -28,6 +32,13 @@ server
       <html lang="en">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="theme-color" content="#4299e1" />
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/public/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/public/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/public/favicon-16x16.png" />
+        <link rel="manifest" href="/public/site.webmanifest" />
+
         <title>Muscogee (Creek) Dictionary</title>
 
         <meta
